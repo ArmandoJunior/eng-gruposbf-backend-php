118f40b93b2542963ef0545e7e7c58e3bf6e5010
Armando N Junior <armandojrn@hotmail.com>
2022-02-13 10:29:44 -0300

    fixed cs warning, removed line.

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,8 +10,6 @@ class Product extends Model
     use HasFactory;
 
     protected $fillable = ['name'];
+    protected $casts = ['id' => 'string'];
 
-//    protected $casts = [
-//        'id' => 'string'
-//    ];
 }
