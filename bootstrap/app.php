fc112e0dc251714ae781a4e77762f97d0337d65b
Armando N Junior <armandojrn@hotmail.com>
2022-02-14 10:53:54 -0300

    fixed test v.7.7.2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,8 +41,6 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
-$app->configure('database');
-
 /*
 |--------------------------------------------------------------------------
 | Return The Application
