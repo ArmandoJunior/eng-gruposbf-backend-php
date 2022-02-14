2ec47b6c41aed92d713389c5bee840f7016e2e37
Armando N Junior <armandojrn@hotmail.com>
2022-02-14 10:19:07 -0300

    fixed test v.7.1

--- tests/Feature/Models/ProductTest.php
+++ tests/Feature/Models/ProductTest.php
@@ -5,12 +5,13 @@ namespace Tests\Feature\Models;
 use App\Models\Brand;
 use App\Models\Category;
 use App\Models\Product;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ProductTest extends TestCase
 {
-    use RefreshDatabase;
+    use RefreshDatabase, DatabaseMigrations;
 
     public function test_get_all(): void
     {
