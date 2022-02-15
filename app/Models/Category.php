7229ca6b44cdb619d51258bd85039b2544bf03d4
Armando N Junior <armandojrn@hotmail.com>
2022-02-15 12:09:26 -0300

    começando a implementar o Redis

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,6 +6,7 @@ use App\Models\Traits\Uuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Cache;
 
 class Category extends Model
 {
