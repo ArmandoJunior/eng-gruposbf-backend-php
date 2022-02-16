723b45ef06a3f91ced4f04ad993383615f48fac6
Armando N Junior <armandojrn@hotmail.com>
2022-02-16 14:11:47 -0300

    improving the swagger documentation and CI improving to {v.16}

--- .github/actions/config-framework/entrypoint.sh
+++ .github/actions/config-framework/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cp .env.example .env
-#cp phpunit.example.xml phpunit.xml
+cp phpunit.example.xml phpunit.xml
 php artisan cache:clear
 php artisan config:clear
 php artisan route:clear
@@ -10,7 +10,6 @@ chmod -R 777 storage bootstrap/cache
 
 #cp /.docker/app/.env .env
 #cp /.docker/app/.env.testing .env.testing
-##cp phpunit.example.xml phpunit.xml
 #php artisan cache:clear
 #php artisan config:clear
 #php artisan route:clear
