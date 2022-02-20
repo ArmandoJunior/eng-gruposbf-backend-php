676387a3fbe12893e4df48bba47c4b123f341d20
Armando N Junior <armandojrn@hotmail.com>
2022-02-20 17:53:46 -0300

    fixed phpcs

--- app/Http/Controllers/PriceController.php
+++ app/Http/Controllers/PriceController.php
@@ -8,7 +8,6 @@ use App\Http\Resources\PriceResource;
 use App\Models\Price;
 use App\Services\Currency\CurrencyApi;
 use App\UseCases\Price\CreatePriceWithCurrencyApiIntegration;
-use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class PriceController extends Controller
 {
@@ -21,8 +20,7 @@ class PriceController extends Controller
     public function store(
         StorePriceRequest $request,
         CreatePriceWithCurrencyApiIntegration $createPriceWithCurrencyApiIntegration
-    )
-    {
+    ) {
         try {
             $fields = json_decode($request->getContent());
             $price = $createPriceWithCurrencyApiIntegration->setData(new PriceDto($fields))->execute();
@@ -39,7 +37,7 @@ class PriceController extends Controller
         $prices = Price::all(['id', 'currency_code']);
 
         try {
-            $prices->filter(function ($price) use ($currencyApi){
+            $prices->filter(function ($price) use ($currencyApi) {
                 $currencyApi->setCurrencyCode($price->currency_code);
 
                 /** Executa a busca do valor de referência para conversão dos preços para a região informada. */
